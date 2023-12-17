e925ae07aa8f6943ab03aac150080e6f8d45569c
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-17 10:16:30 +0000

    Add files via upload

---
+++ GridSystem_tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import*
+form=Tk()
+form.geometry("700x500")
+
+lbl1=Label(form,text="label 1",font =("None 20 bold"))
+lbl1.grid(row =0,column=0)
+
+btn1 =Button(form ,text ="clich here",font =("None 20 bold"))
+btn1.grid(row=1,column=0)
